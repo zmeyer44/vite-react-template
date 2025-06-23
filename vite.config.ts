3bedd935a3db3e773ee003a4dac3b82805c73ad7
zmeyer44 <zmmeyer44@gmail.com>
2025-06-23 11:14:10 -0400

    trying icon stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      // e.g. use TypeScript check
+      typescript: true,
+    }),
+  ],
+  // The purpose of this configuration is to prevent Vite from pre-bundling the "lucide-react" package during the dependency optimization process.
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
-})
+});
